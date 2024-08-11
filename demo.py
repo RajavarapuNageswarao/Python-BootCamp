394643537f094e3deec3510b0d7598813bde0576
RajavarapuNageswarao <rajavarapumunna@gmail.com>
2024-08-11 11:51:54 +0530

    This is my first project

---
+++ demo.py
@@ -0,0 +1,8 @@
+#pop is the delete the last word,append is the insert one word at last
+#particular is added is the list using [0] = "john" ->This is the particular word
+my_list = ["john",1,23,"doe"]
+#my_new_list = my_list[0] = "update"
+new_list = my_list.pop() 
+#print(new_list)
+print(my_list)
+print(len(my_list))
